e03a25f03d76988085f7737f8bb94400b5a5b4dc
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-09-29 00:08:09 +0900

    wip: リモートコンテナ接続

--- README.md
+++ README.md
@@ -5,12 +5,14 @@ JavaEEサンプル
 
 ```sh
 # テスト実行
-./gradlew clean test -i
+./gradlew clean build -i
 ```
 
 参考
 ----
 
+* [arquillian glassfish EJB - Google 検索](https://www.google.co.jp/search?q=arquillian+glassfish+EJB&newwindow=1&source=lnt&tbs=lr:lang_1ja&lr=lang_ja&sa=X&ved=0ahUKEwjD6YSFh8jWAhWFJZQKHfEPAmwQpwUIHw&biw=1304&bih=702)
+* [結合テスト - Web開発芸術 Learn Once, Practice Anywhere.](https://sites.google.com/site/webdevelopart/30-common/integration-test)
 * [Reference Guide - Arquillian - Project Documentation Editor](https://docs.jboss.org/author/display/ARQ/Reference+Guide)
 * [Multiple Containers - Arquillian - Project Documentation Editor](https://docs.jboss.org/author/display/ARQ/Multiple+Containers)
 * [GlassFish 3.1 - Remote - Arquillian - Project Documentation Editor](https://docs.jboss.org/author/display/ARQ/GlassFish+3.1+-+Remote)
--- SampleTest/src/test/java/com/github/tkmtmkt/ParallelSampleTest.java
+++ SampleTest/src/test/java/com/github/tkmtmkt/ParallelSampleTest.java
@@ -1,37 +1,51 @@
 package com.github.tkmtmkt;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.io.File;
-import javax.ejb.EJB;
+
+import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class ParallelSampleTest {
-    @EJB
+    //@EJB(mappedName = "java:global/SampleEAR-1.0-SNAPSHOT/SampleEJB-1.0-SNAPSHOT/ParallelSample")
+    //@EJB(mappedName = "java:global/SampleEAR-1.0-SNAPSHOT/SampleEJB-1.0-SNAPSHOT/ParallelSample!com.github.tkmtmkt.ParallelSample")
+    @Inject
     private ParallelSample sut;
 
     @Deployment
-    public static EnterpriseArchive createDeployment() {
-        final EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class, "test.ear")
-            .as(ZipImporter.class)
-            .importFrom(new File("../SampleEAR/build/libs/SampleEAR-1.0-SNAPSHOT.ear"))
-            .as(EnterpriseArchive.class);
-        final JavaArchive testLibrary = ShrinkWrap.create(JavaArchive.class, "testLibrary.jar")
-            .addClass(ParallelSampleTest.class);
-        ear.addAsLibrary(testLibrary);
+    public static Archive<?> createDeployment() {
+        final EnterpriseArchive ear = ShrinkWrap
+                .createFromZipFile(EnterpriseArchive.class, new File("../SampleEAR/build/libs/SampleEAR-1.0-SNAPSHOT.ear"));
+
+        //ArquillianServletRunner
+        final WebArchive war = ShrinkWrap.create(WebArchive.class, "arquillian-test.war")
+                .addPackages(true, "com.github.tkmtmkt")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"));
+        ear.addAsModule(war);
+
+        System.out.println(war.toString(true));
+        System.out.println(ear.toString(true));
 
         return ear;
     }
 
     @Test()
+    //@RunAsClient
     public void testExecute() {
+        System.out.println(">>>>> テスト実行");
+        assertThat(sut).isNotNull();
         sut.execute();
     }
 }
