54b7c2887f46bc261eec0b187b50c9736d5b9f60
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-09-28 11:28:28 +0900

    wip: Deploy

--- SampleTest/src/test/java/com/github/tkmtmkt/ParallelSampleTest.java
+++ SampleTest/src/test/java/com/github/tkmtmkt/ParallelSampleTest.java
@@ -1,10 +1,13 @@
 package com.github.tkmtmkt;
 
+import java.io.File;
 import javax.ejb.EJB;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,9 +18,16 @@ public class ParallelSampleTest {
     private ParallelSample sut;
 
     @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class, "test.jar")
-            .addClass(ParallelSample.class);
+    public static EnterpriseArchive createDeployment() {
+        final EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class, "test.ear")
+            .as(ZipImporter.class)
+            .importFrom(new File("../SampleEAR/build/libs/SampleEAR-1.0-SNAPSHOT.ear"))
+            .as(EnterpriseArchive.class);
+        final JavaArchive testLibrary = ShrinkWrap.create(JavaArchive.class, "testLibrary.jar")
+            .addClass(ParallelSampleTest.class);
+        ear.addAsLibrary(testLibrary);
+
+        return ear;
     }
 
     @Test()
