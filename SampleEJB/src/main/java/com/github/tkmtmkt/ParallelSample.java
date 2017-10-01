fdab1028faccf0fd1c8fbe98b7cdc9ca50ac0e68
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-10-01 23:13:53 +0900

    wip: @Remoteインタフェース追加

--- SampleEJB/src/main/java/com/github/tkmtmkt/ParallelSample.java
+++ SampleEJB/src/main/java/com/github/tkmtmkt/ParallelSample.java
@@ -1,81 +1,8 @@
 package com.github.tkmtmkt;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import javax.ejb.Remote;
 
-import javax.annotation.Resource;
-import javax.ejb.Stateless;
-import javax.enterprise.concurrent.ManagedExecutorService;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@Stateless
-public class ParallelSample {
-    final static Logger logger = LoggerFactory.getLogger(ParallelSample.class);
-
-    final Random rnd = new Random();
-
-    @Resource
-    ManagedExecutorService executor;
-
-    public void execute() {
-        logger.info(">>>>> execute() start");
-
-        //タスク一覧
-        List<Supplier<Void>> taskList = new ArrayList<Supplier<Void>>(){{
-            add(() -> task1());
-            add(() -> task2());
-        }};
-
-        //並列処理
-        List<CompletableFuture<Void>> futureList =
-            taskList.stream()
-                .map(task -> CompletableFuture.supplyAsync(task, executor))
-                .collect(Collectors.toList());
-
-        CompletableFuture
-            //リストから配列に変換
-            .allOf(futureList.toArray(new CompletableFuture[futureList.size()]))
-            //全ての処理が完了したら
-            .whenComplete((Void result, Throwable e) -> {
-                if (e != null) {
-                    logger.error(e.getMessage(), e);
-                }
-                logger.info("call whenComplete");
-            }).join();
-
-        logger.info(">>>>> execute() end");
-    }
-
-    private Void task1() {
-        IntStream.range(0, 10).forEach((i) -> {
-            try {
-                logger.info("task1 ({})", i);
-                Thread.sleep(rnd.nextInt(1000));
-            } catch (InterruptedException e) {
-                logger.error("task1", e);
-            }
-        });
-        logger.info("task1 completed!");
-        return null;
-    }
-
-    private Void task2() {
-        IntStream.range(0, 10).forEach((i) -> {
-            try {
-                logger.info("task2 ({})", i);
-                Thread.sleep(rnd.nextInt(1000));
-            } catch (InterruptedException e) {
-                logger.error("task2", e);
-            }
-        });
-        logger.info("task2 completed!");
-        return null;
-    }
+@Remote
+public interface ParallelSample {
+    public void execute();
 }
