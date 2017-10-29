d2849c9263937d430ef64c24e2e100069252678a
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-10-29 23:13:34 +0900

    wip: リモートテストのNG対応
    * 名前解決できるように指定
    * 接続先の物理情報をglassfish-ejb-jar.xmlに記述しているが、これを
      削除して実行環境側で指定するようにするほうがよい。（要件等）

--- jars/AppleEJB/src/main/java/com/github/tkmtmkt/Apple.java
+++ jars/AppleEJB/src/main/java/com/github/tkmtmkt/Apple.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @LocalBean
 @Stateless
 public class Apple implements AppleRemote {
-    @EJB
+    @EJB(name="ejb/OrangeRemote")
     OrangeRemote orange;
 
     @Override
